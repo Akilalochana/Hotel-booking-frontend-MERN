d3524bb0697db28065d73b753cdb4fed7447dbb2
Akila Lochana <168577881+Akilalochana@users.noreply.github.com>
2025-04-22 01:10:51 +0530

    add react icons and created admin dashbord sample UI📱

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import AdminDashboard from './components/adminDashboard'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      
-    </>
+   <div>
+    <AdminDashboard/>
+   </div>
   )
 }
 
---
+++ src/components/productCard.jsx
@@ -0,0 +1,7 @@
+export default function ProductCard(){
+  return(
+    <div>
+      <h1>room 1</h1>
+    </div>
+  )
+}
