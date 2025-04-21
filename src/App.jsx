f1a30cb914a3ccd6d3321598d0b2102f91d91e0d
Akila Lochana <168577881+Akilalochana@users.noreply.github.com>
2025-04-22 02:20:14 +0530

    add react router dom and routes pages⬅️➡️

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import AdminDashboard from './components/adminDashboard'
+import AdminDashboard from './pages/admin/adminDashboard'
 
 
 function App() {
-  
-
   return (
-   <div>
-    <AdminDashboard/>
-   </div>
+  
+  <BrowserRouter>
+     <Routes path="/*">
+        <Route path='/admin/*' element={<AdminDashboard/>}/>
+        <Route path='/' element={<h1>Home</h1>}/>
+        <Route path='/*' element={<h1>404 NOT Found</h1>}/>
+
+     </Routes>
+  </BrowserRouter>
   )
 }
 
---
+++ src/pages/admin/adminDashboard.jsx
@@ -0,0 +1,58 @@
+import { FaHotel } from "react-icons/fa6";
+import { BsGraphUp } from "react-icons/bs";
+import { FaBookBookmark } from "react-icons/fa6";
+import { MdBedroomChild } from "react-icons/md";
+import { MdRateReview } from "react-icons/md";
+import { FaUserCog } from "react-icons/fa";
+import { Link, Route, Routes } from "react-router-dom";
+
+export default function AdminDashboard() {
+  return(
+    <div className='w-full h-screen flex'>
+
+    <div className='w-[400px] h-full border-[#bbb] border-r-2 '>
+      <div className="flex h-[50px] font-bold text-2xl justify-center items-center mb-10">
+         Admin Page
+      </div>
+      
+      
+      <button className='w-full h-[50px] text-2xl font-bold  flex justify-center items-center gap-1'>
+        <BsGraphUp className='text-xl'/>
+        Dashboard
+      </button>
+
+      <Link to="/admin/booking" className='w-full h-[50px] text-2xl font-bold   flex justify-center items-center gap-1'>
+        <FaBookBookmark/>
+        Bookings
+      </Link>
+
+      <Link to='/admin/rooms' className='w-full h-[50px] text-2xl font-bold   flex justify-center items-center gap-1'>
+        <MdBedroomChild/>
+        Rooms
+      </Link>
+
+      <Link to='/admin/reviews' className='w-full h-[50px] text-2xl font-bold   flex justify-center items-center gap-1'>
+        <MdRateReview/>
+        Reviews
+      </Link>
+
+      <Link to='/admin/users' className='w-full h-[50px] text-2xl font-bold   flex justify-center items-center gap-1'>
+        <FaUserCog/>
+        Users
+      </Link>
+
+    </div>
+
+    <div className='w-[calc(100vw-400px)] h-full '>
+      <Routes path="/*">
+          <Route path="/booking" element={<h1>Booking</h1>}/>
+          <Route path="/rooms" element={<h1>Rooms</h1>}/>
+          <Route path="/reviews" element={<h1>Reviews</h1>}/>
+          <Route path="/users" element={<h1>Users</h1>}/>
+
+      </Routes>
+      
+    </div>
+  </div>
+  )
+}
